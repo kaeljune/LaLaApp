f9f4fad97b826c27d956bdd8eab5b2ba6039649c
Hiep Tran <hieptran@acocean.com>
2017-10-25 10:42:22 +0700

    .

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import Reactotron from 'reactotron-react-native';
+//import Reactotron from 'reactotron-react-native';
 import { compose, applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { persistStore, autoRehydrate } from 'redux-persist';
@@ -7,8 +7,8 @@ import reducers from '../reducers';
 
 // import '../../ReactotronConfig';
 
-//const store = createStore(
-const store = Reactotron.createStore(
+const store = createStore(
+//const store = Reactotron.createStore(
   reducers,
   {},
   compose(
